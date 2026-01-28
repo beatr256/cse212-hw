12945aa620a55cf67bf081f2d39bdf40d9170a2b
beatr256 <hapolot@byupathway.edu>
2026-01-28 04:57:12 -0800

    push now

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -1,20 +1,38 @@
+using System;
 using System.Collections.Generic;
 
 public class Translator
 {
-    private Dictionary<string, string> translations =
-        new Dictionary<string, string>();
+    private Dictionary<string, string> _words = new();
 
-    public void AddWord(string fromWord, string toWord)
+    public static void Run()
     {
-        translations[fromWord] = toWord;
+        var englishToGerman = new Translator();
+        englishToGerman.AddWord("House", "Haus");
+        englishToGerman.AddWord("Car", "Auto");
+        englishToGerman.AddWord("Plane", "Flugzeug");
+
+        Console.WriteLine(englishToGerman.Translate("Car"));   // Auto
+        Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
+        Console.WriteLine(englishToGerman.Translate("Train")); // ???
     }
 
-    public string Translate(string word)
+    /// <summary>
+    /// Add the translation from 'fromWord' to 'toWord'
+    /// </summary>
+    public void AddWord(string fromWord, string toWord)
     {
-        if (translations.ContainsKey(word))
+        _words[fromWord] = toWord;
+    }
+
+    /// <summary>
+    /// Translates a word or returns "???" if not found
+    /// </summary>
+    public string Translate(string fromWord)
+    {
+        if (_words.ContainsKey(fromWord))
         {
-            return translations[word];
+            return _words[fromWord];
         }
 
         return "???";
